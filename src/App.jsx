28e7625e248269155ce6a21adffd469e8a8c1500
Anatoliy GALKIN <tolik.galkin1456@mail.ru>
2026-04-14 14:52:06 +0300

    Обновление проекта: удалены старые файлы, добавлены новые

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,15 @@
 import './App.css'
-import Button from './components/Button/Button'
-import CardButton from './components/CardButton/CardButton'
-import JournalItem from './components/JournalItem/JournalItem'
+import Header from './components/Header/Header.jsx'
+import Text from './components/Text/Text.jsx'
+import Button from './components/Button/Button.jsx'
 
 function App() {
 
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Горные походы открывают удивительные природные ландшафты'
-    },
-    {
-      title: 'Поход в горы',
-      date: new Date(),
-      text: 'Думал, что очень много времени'
-    },
-  ]
-
   return (
     <>
-      <h1>Заголовок</h1>
-      <p>Текст</p>
-      <Button />
-      <CardButton>Новое воспоминание</CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[0].title}
-          text={data[0].text}
-          date={data[0].date}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[1].title}
-          text={data[1].text}
-          date={data[1].date}
-        />
-      </CardButton>
+   <Header/>
+   <Text/>
+   <Button/>
     </>
   )
 }
---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,11 @@
+import './Text.css'
+
+function Text(){
+return(
+    <>
+    <p className='text'>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</p>
+    </>
+)
+}
+
+export default Text
