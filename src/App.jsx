755009a24a74bfcc99636f91797f51d58241cc4a
Anatoliy GALKIN <tolik.galkin1456@mail.ru>
2026-06-04 15:17:11 +0300

    add favorite button and movie list

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,53 @@ import SearchInput from './components/SearchInput/SearchInput.jsx'
 import Menu from './components/Menu/Menu.jsx'
 import MenuPanel from './components/MenuPanel/MenuPanel.jsx'
 import MenuButton from './components/MenuButton/MenuButton.jsx'
+import MovieList from './components/MovieList/MovieList.jsx'
+import MovieItem from './components/MovieItem/MovieItem.jsx'
 
 function App() {
+
+  const movieData = [
+    {
+      title: 'Black Widow',
+      image: 'image1',
+      rating: '324'
+    },
+    {
+      title: 'Shang Chi',
+      image: 'image2',
+      rating: '124'
+    },
+    {
+      title: 'Loki',
+      image: 'image3',
+      rating: '235'
+    },
+    {
+      title: 'How I Met Your Mother',
+      image: 'image4',
+      rating: '235'
+    },
+    {
+      title: 'Money Heist',
+      image: 'image5',
+      rating: '8125'
+    },
+    {
+      title: 'Friends',
+      image: 'image6',
+      rating: '123'
+    },
+    {
+      title: 'The Big Bang Theory',
+      image: 'image7',
+      rating: '12'
+    },
+    {
+      title: 'Two And a Half Men',
+      image: 'image8',
+      rating: '456'
+    },
+  ]
 
   return (
     <>
@@ -25,6 +70,15 @@ function App() {
         <SearchInput />
         <Button text='Искать' />
       </Search>
+      <MovieList>
+        {movieData.map((movie, index) =>
+          <MovieItem
+            key={index}
+            title={movie.title}
+            image={movie.image}
+            rating={movie.rating} />
+        )}
+      </MovieList>
     </>
   )
 }
---
+++ src/components/AddFavoriteButton/AddFavoriteButton.jsx
@@ -0,0 +1,11 @@
+import './AddFavoriteButton.css'
+
+function AddFavoriteButton(){
+return(
+    <>
+    <button className='add-favorite-button'>В избранное</button>
+    </>
+)
+}
+
+export default AddFavoriteButton
